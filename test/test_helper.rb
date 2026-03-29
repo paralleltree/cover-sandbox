0963e99bee02461bdfb0333aaf263ff5c254f3ca
Paralleltree <paralleltree@outlook.com>
2026-03-29 15:57:10 +0900

    Change loading order of SimpleCov in test_helper
    アプリケーションコードより先にSimpleCovを読み込んでいないことで
    カバレッジが正しく計測されなかった点を修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
-require "minitest/autorun"
-require "minitest-spec-context"
 require "simplecov"
 
 SimpleCov.start do
   add_filter "test/"
   enable_coverage :branch
 end
+
+require "minitest/autorun"
+require "minitest-spec-context"
